45e4d8d4aece63fce027732af2888bdc70730f64
Raveena Ingale <ingale.r@northeastern.edu>
2025-04-22 20:15:16 -0400

    load data in pig

--- scripts/run_pig_jobs.sh
+++ scripts/run_pig_jobs.sh
@@ -2,32 +2,34 @@
 
 # Script to run the Pig jobs in sequence
 # Usage: ./run_pig_jobs.sh
-# Permission to make it executable as shell script: 
+# Permission to make it executable as shell script:
 # chmod +x run_pig_jobs.sh
 
+# Set permission to write to logs
+LOG_DIR="$(pwd)/logs"
+mkdir -p $LOG_DIR
 
 # Create HDFS directories if they don't exist
-hadoop fs -mkdir -p /crime_data_analysis/raw
-hadoop fs -mkdir -p /crime_data_analysis/output
-hadoop fs -mkdir -p /crime_data_analysis/processed
-hadoop fs -mkdir -p /crime_data_analysis/analyzed
-
+# hadoop fs -mkdir -p /crime_data_analysis/raw
+# hadoop fs -mkdir -p /crime_data_analysis/output
+# hadoop fs -mkdir -p /crime_data_analysis/processed
+# hadoop fs -mkdir -p /crime_data_analysis/analyzed
 
 # Copy the data file to HDFS if not already there
-echo "Copying data to HDFS if needed..."
-hadoop fs -test -e /crime_data_analysis/raw/crime_data.csv
-if [ $? -ne 0 ]; then
-    echo "Data file not found in HDFS, copying from local..."
-    hadoop fs -copyFromLocal /Users/ingale.r/Downloads/Crime_Data_from_2020_to_Present.csv /crime_data_analysis/raw/crime_data.csv
-fi
+# echo "Copying data to HDFS if needed..."
+# hadoop fs -test -e /crime_data_analysis/raw/crime_data.csv
+# if [ $? -ne 0 ]; then
+#     echo "Data file not found in HDFS, copying from local..."
+#     hadoop fs -copyFromLocal /Users/ingale.r/Downloads/Crime_Data_from_2020_to_Present.csv /crime_data_analysis/raw/crime_data.csv
+# fi
 
 # Run the load job
-# echo "Running data load job..."
-# pig -x mapreduce -f pig-scripts/load/load_crime_data.pig > $LOG_DIR/load_$(date +%Y%m%d_%H%M%S).log 2>&1
-# if [ $? -ne 0 ]; then
-#     echo "ERROR: Data load job failed!"
-#     exit 1
-# fi
+echo "Running data load job..."
+pig -x mapreduce -f $(pwd)/pig-scripts/load/load_crime_data.pig > $LOG_DIR/load_$(date +%Y%m%d_%H%M%S).log 2>&1
+if [ $? -ne 0 ]; then
+    echo "ERROR: Data load job failed!"
+    exit 1
+fi
 
 # Run the cleanup job
 # echo "Running data cleanup job..."
@@ -44,4 +46,3 @@ fi
 #     echo "ERROR: Data transform job failed!"
 #     exit 1
 # fi
-
